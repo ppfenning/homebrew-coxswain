b7438f72987408c10b2d7524cb394530c3c0a198
Patrick Pfenning <pfenningpat@gmail.com>
2026-09-25 22:20:21 -0400

    cox 0.18.0

--- Formula/cox.rb
+++ Formula/cox.rb
@@ -8,8 +8,8 @@ class Cox < Formula
 
   desc "Operator CLI for Coxswain: run records, traces, landing, and the live screens"
   homepage "https://ppfenning.github.io/coxswain/latest/"
-  url "https://files.pythonhosted.org/packages/b2/e0/9194a791d53fd34351848731e8f62bdcedeb733bdc50d1e1a6d2d0a8fdaf/coxswain_tools-0.17.0.tar.gz"
-  sha256 "a43b4da94c35a4bcb6dab1cd961daa3fbb3a81c4f4c3d1bf0e2e809fb9005f6c"
+  url "https://files.pythonhosted.org/packages/26/70/9504a26cbd7dc34aab6c303a6ecd4166c2d721d3a624336dab7784f128b7/coxswain_tools-0.18.0.tar.gz"
+  sha256 "82b46014badd679dec32b8f88db796c0aec5f05c9e4342ff714c79d8c5dfcf78"
   license "MIT"
   head "https://github.com/ppfenning/coxswain-tools.git", branch: "main"
 
